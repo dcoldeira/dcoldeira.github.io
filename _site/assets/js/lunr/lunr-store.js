5ff8bd01b4557f2e0fc653af0adf29972dcde432
dcoldeira <dcoldeira@gmail.com>
2023-04-17 18:32:42 +0100

    new content

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "ESP32 LED Control",
+        "excerpt":"This project shows some of the features of the powerful ESP32 microcontroller. Starting with a simple LED circuit, we demonstrate how to control it using code and how to toggle it on and off with a push button. We then use pulse-width modulation (PWM) to control the LED’s brightness and...","categories": ["embedded"],
+        "tags": ["ESP32","C++","electronics"],
+        "url": "/embedded/ESP32-LED-Control/",
+        "teaser": null
+      },{
+        "title": "IoT Smart Light using an ESP32 and Flutter",
+        "excerpt":"Dart and Flutter for Embedded Systems Dart is a modern, object-oriented programming language created by Google. It’s fast, efficient, and easy to learn. Flutter, on the other hand, is a UI toolkit built on top of Dart that allows you to create beautiful, native applications for mobile, web, and desktop....","categories": ["projects"],
+        "tags": ["embedded systems","Dart","mobile app","Flutter","ESP32"],
+        "url": "/projects/Building-Embedded-Systems-with-Dart-and-Flutter/",
+        "teaser": null
+      },{
+        "title": "Weather Station with ESP32 WROOM 32D and BMP180 Sensor",
+        "excerpt":"In this project, we have built a weather station using an ESP32 board and a BMP180 sensor. Our goal was to create a custom weather station that is tailored to our specific needs, and we have achieved this by combining the powerful capabilities of the ESP32 board with the accurate...","categories": ["projects"],
+        "tags": ["ESP32","BMP180","weather station","IoT"],
+        "url": "/projects/ESP32-Temperature-Sensor/",
+        "teaser": null
+      }]
